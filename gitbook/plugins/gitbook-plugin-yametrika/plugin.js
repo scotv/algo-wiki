c7040f2bb16307655b34958c67dba6fe99cb95b5
scott@c9 <scozv@yandex.com>
2015-07-31 10:01:33 +0000

    Updates

--- gitbook/plugins/gitbook-plugin-yametrika/plugin.js
+++ gitbook/plugins/gitbook-plugin-yametrika/plugin.js
@@ -1,10 +1,10 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
-        config.ga = config.ga || {};
+        config.yametrika = config.yametrika || {};
     });
 
     gitbook.events.bind("page.change", function() {
-        ga('send', 'pageview', window.location.pathname+window.location.search);
+        var yaCounter"+config.number+" = new Ya.Metrika({id: "+config.number+"});
     });
 
     gitbook.events.bind("exercise.submit", function(e, data) {
